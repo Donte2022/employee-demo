95568d7291bb5a370049db4067c7d7f6c5895f10
Dontavious Green <dontavious.o.green@gmail.com>
2022-11-03 09:32:44 -0500

    init

---
+++ src/main/java/net/yorksolutions/dontaviousgreencontactprjbe/DontaviousGreenContactPrjBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.dontaviousgreencontactprjbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontaviousGreenContactPrjBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DontaviousGreenContactPrjBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/dontaviousgreencontactprjbe/DontaviousGreenContactPrjBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.dontaviousgreencontactprjbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontaviousGreenContactPrjBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
